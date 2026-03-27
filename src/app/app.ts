d8746deee09f94b8cc4a3e9af6241a68217af0f4
Ryan <ryanak@gmail.com>
2026-03-26 17:23:47 -0700

    more recipes, don't show link to add recipe on add recipe page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
 import { Component, signal,  } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { computed } from '@angular/core';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +11,18 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-    protected readonly title = signal<String>('My Recipe Box')
+    protected readonly title = signal<String>('My Recipe Box');
+    protected readonly currentUrl = signal('');
+    
+    constructor(private router: Router) {
+        this.router.events.pipe(
+            filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+        ).subscribe(event => {
+            this.currentUrl.set(event.urlAfterRedirects);
+        });
+    }
+    
+    protected readonly isAddRecipeRoute = computed(() => {
+        return this.currentUrl().includes('/recipes/add');
+    });
 }
